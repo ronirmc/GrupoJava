b2d953811e95814a21a04f1d3a9be2eaa27c4797
ronirmc <carmoronim@gmail.com>
2013-11-18 11:30:23 -0200

    Segundo teste

--- src/teste/PrimeiroTeste.java
+++ src/teste/PrimeiroTeste.java
@@ -3,6 +3,10 @@ package teste;
 public class PrimeiroTeste {
 	public static void main(String[] args) {
 		System.out.println("Primeiro Test");
+		
+		System.err.println("Segundo teste");
 	}
+	
+	
 
 }
