b686a2e4541ae814e25b629aad1520d9a6bb120f
ronirmc <carmoronim@gmail.com>
2013-11-23 11:13:24 -0200

    Primeira Janela

---
+++ src/interfacesGraficas/Principal.java
@@ -0,0 +1,18 @@
+
+package interfacesGraficas;
+
+/**
+ *
+ * @author Roni
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicializado");
+        new TelaCadastroMaterial().setVisible(true);
+        
+    }
+}
